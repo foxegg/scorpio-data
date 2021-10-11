3b658f68553e87beefaa210e8558a1a8823ef065
luolaigang <luolaigang@newstarfintech.com>
2021-10-11 17:29:34 +0800

    图片保存

--- scorpiodata/src/main/java/com/newstar/scorpiodata/utils/PictureUtils.java
+++ scorpiodata/src/main/java/com/newstar/scorpiodata/utils/PictureUtils.java
@@ -2,6 +2,7 @@ package com.newstar.scorpiodata.utils;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -9,6 +10,7 @@ import android.media.ExifInterface;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Log;
 
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
@@ -296,5 +298,47 @@ public class PictureUtils {
         height = height / inSampleSize;
 
         return width * height * perPixBytes;
+    }
+
+    /**
+     * Save Bitmap
+     *
+     * @param name file name
+     * @param bm  picture to save
+     */
+    public static String saveBitmap(String name, Bitmap bm, Context mContext) {
+        //指定我们想要存储文件的地址
+        String TargetPath = mContext.getFilesDir() + "/images/";
+        //判断指定文件夹的路径是否存在
+        if (!fileIsExist(TargetPath)) {
+            Log.d("Save Bitmap", "TargetPath isn't exist");
+        } else {
+            //如果指定文件夹创建成功，那么我们则需要进行图片存储操作
+            File saveFile = new File(TargetPath, name);
+            try {
+                FileOutputStream saveImgOut = new FileOutputStream(saveFile);
+                // compress - 压缩的意思
+                bm.compress(Bitmap.CompressFormat.JPEG, 80, saveImgOut);
+                //存储完成后需要清除相关的进程
+                saveImgOut.flush();
+                saveImgOut.close();
+                Log.d("Save Bitmap", "The picture is save to your phone!"+saveFile.getAbsolutePath());
+                return saveFile.getAbsolutePath();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+    static boolean fileIsExist(String fileName)
+    {
+        //传入指定的路径，然后判断路径是否存在
+        File file=new File(fileName);
+        if (file.exists())
+            return true;
+        else{
+            //file.mkdirs() 创建文件夹的意思
+            return file.mkdirs();
+        }
     }
 }
