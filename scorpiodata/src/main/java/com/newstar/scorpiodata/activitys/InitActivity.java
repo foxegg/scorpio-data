973b4c86c9d062a9d08f80913eea084e851770d5
luolaigang <luolaigang@newstarfintech.com>
2022-02-17 15:33:26 +0800

    获取fmctoken

--- scorpiodata/src/main/java/com/newstar/scorpiodata/activitys/InitActivity.java
+++ scorpiodata/src/main/java/com/newstar/scorpiodata/activitys/InitActivity.java
@@ -2,11 +2,19 @@ package com.newstar.scorpiodata.activitys;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.newstar.scorpiodata.utils.SharedHelp;
 import com.newstar.scorpiodata.utils.ViewHelp;
+
+import java.util.Objects;
 
 public abstract class InitActivity extends AppCompatActivity implements LivenessHelp {
     public boolean lightBar;
@@ -16,7 +24,33 @@ public abstract class InitActivity extends AppCompatActivity implements Liveness
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
         ViewHelp.setFullscreen(this, lightBar);
+        handler.sendEmptyMessage(0);
     }
+
+    public void getFirebaseMessagingToken ( ) {
+        FirebaseMessaging.getInstance ().getToken ()
+                .addOnCompleteListener ( task -> {
+                    if (!task.isSuccessful ()) {
+                        handler.sendEmptyMessageDelayed(0,1000);
+                        //Could not get FirebaseMessagingToken
+                        return;
+                    }
+                    if (null != task.getResult ()) {
+                        //Got FirebaseMessagingToken
+                        String firebaseMessagingToken = Objects.requireNonNull ( task.getResult () );
+                        //Use firebaseMessagingToken further
+                        SharedHelp.setSharedPreferencesValue(SharedHelp.FMC_TOKEN,firebaseMessagingToken);
+                    }
+                } );
+    }
+
+    Handler handler  = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            getFirebaseMessagingToken();
+        }
+    };
 
     @Override
     public void setContentView(int layoutResID) {
