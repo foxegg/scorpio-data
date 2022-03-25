376653149bb8a424e41400587730cc69fb20a614
luolaigang <luolaigang@newstarfintech.com>
2022-03-25 10:02:01 +0800

    添加打开前置摄像头方法

--- scorpiodata/src/main/java/com/newstar/scorpiodata/BaseApplication.java
+++ scorpiodata/src/main/java/com/newstar/scorpiodata/BaseApplication.java
@@ -30,18 +30,18 @@ public class BaseApplication extends Application {
 
     }
 
-    public void initKochava(String kochavaGuid){
-        i("luolaigang","initKochava");
+    public void initKochava(String kochavaGuid) {
+        Log.i("luolaigang", "initKochava");
         // Start the Kochava Tracker
         Tracker.Configuration configuration = new Tracker.Configuration(this);
         configuration.setAppGuid(kochavaGuid);
         Tracker.configure(configuration.setAttributionUpdateListener(attribution -> {
-            i("luolaigang","call back");
+                    Log.i("luolaigang", "call back");
                     // got the attribution results, now we need to parse it
                     try {
                         JSONObject attributionObject = new JSONObject(attribution);
                         SharedHelp.setSharedPreferencesValue(SharedHelp.KOCHAVE_REFERRER_URL, attribution);
-                        if("false".equals(attributionObject.optString("attribution", ""))) {
+                        if ("false".equals(attributionObject.optString("attribution", ""))) {
                             // Install is not attributed.
                         } else {
                             // Install is attributed. Retrieve the values we care about.
@@ -50,9 +50,9 @@ public class BaseApplication extends Application {
                             //Kochave kochave = new Gson().fromJson(attribution, Kochave.class);
                             //Log.i("luolaigangTracker",kochave.getCampaign());
                         }
-                        i("luolaigang",attribution);
+                        Log.i("luolaigang", attribution);
                     } catch (JSONException exception) {
-                        i("luolaigang",exception.getMessage());
+                        Log.i("luolaigang", exception.getMessage());
                     }
                 })
         );
@@ -60,18 +60,19 @@ public class BaseApplication extends Application {
     }
 
     static int LENGTH = 4000;
+
     public static void i(String tag, String msg) {
-        if(BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             if (msg.length() > LENGTH) {
                 for (int i = 0; i < msg.length(); i += LENGTH) {
                     if (i + LENGTH < msg.length()) {
-                        android.util.Log.i(tag, msg.substring(i, i + LENGTH));
+                        Log.i(tag, msg.substring(i, i + LENGTH));
                     } else {
-                        android.util.Log.i(tag, msg.substring(i, msg.length()));
+                        Log.i(tag, msg.substring(i, msg.length()));
                     }
                 }
             } else {
-                android.util.Log.i(tag, msg);
+                Log.i(tag, msg);
             }
         }
     }
@@ -83,7 +84,7 @@ public class BaseApplication extends Application {
                 try {
                     AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(getApplicationContext());
                     String adid = info.getId();
-                    LogUtils.i( adid);
+                    LogUtils.i(adid);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
